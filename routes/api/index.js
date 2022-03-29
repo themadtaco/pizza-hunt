8c45d68d701fe83486f051a9ab39fabf204318e7
themadtaco <aatrox48@gmail.com>
2022-03-29 09:43:39 -0500

    finished comment models

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commentRouters = require('./comment-routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+router.use('/comments', commentRouters);
 
 module.exports = router;
